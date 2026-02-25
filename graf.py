764f11e0f04c57949be9d7f47ab1ad42eb8f8b84
victor <victorpsena2015@gmail.com>
2026-02-25 17:33:10 -0300

    Corrigindo tópicos da dissertação até SGD

--- graf.py
+++ graf.py
@@ -1,27 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Criando uma matriz 28x28 com valores de 0 a 255
-matriz = np.random.randint(0, 256, (10, 10))
-fig, ax = plt.subplots()
 
-# Exibe a matriz como imagem em escala de cinza
-im = ax.imshow(matriz, cmap='gray', vmin=0, vmax=255)
+def plot_graf(x, y, title='Graf', output_path='graf.png') -> str:
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(title)
+    plt.grid(False)
+    plt.savefig(output_path, dpi=300, bbox_inches='tight')
+    plt.close()
+    return output_path
 
-# Adiciona os valores dentro de cada célula
-for i in range(matriz.shape[0]):
-    for j in range(matriz.shape[1]):
-        ax.text(j, i, matriz[i, j],
-                ha='center', va='center',
-                color='red')
 
-plt.colorbar(im)
-# plt.title(f"Matriz em Escala de Cinza ({matriz.shape[0]}x{matriz.shape[1]})")
+x = np.linspace(-10, 10, 100)
+y = x**2
 
-backend = plt.get_backend().lower()
-if "agg" in backend:
-    output_file = "matriz_escala_cinza.png"
-    plt.savefig(output_file, dpi=300, bbox_inches="tight")
-    print(f"Backend não interativo detectado ({backend}). Figura salva em: {output_file}")
-else:
-    plt.show()
+figure = plot_graf(x, y, title=r'')
+print(f'Figura salva em: {figure}')

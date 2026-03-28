40c40570c9a70f2ce1ba9f02549c6aee0a2e39aa
victor <victorpsena2015@gmail.com>
2026-03-28 14:39:43 -0300

    Modificando a seção de convolução e adicionando a referência do BENGIO

--- graf.py
+++ graf.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 def plot_graf(x, y, title='Graf', output_path='graf.png', pontosBanco=None, pontosTreino = None) -> str:
     plt.plot(x, y, label='Rede Neural', color='green')
-    plt.xlabel('x', fontsize=16)
-    plt.ylabel('y', fontsize=16)
+    plt.xlabel(r'$z$', fontsize=16)
+    plt.ylabel(r"$\sigma'(z)$", fontsize=16)
     plt.title(title, fontsize=18)
     plt.grid(False)
     if pontosBanco:
         for i, ponto in enumerate(pontosBanco):
             plt.scatter(*ponto, color='red', label='banco de treino' if i == 0 else "")
-        plt.legend()
+        # plt.legend()
     if pontosTreino:
         for i, ponto in enumerate(pontosTreino):
             plt.scatter(*ponto, color='blue', label='banco de teste' if i == 0 else "")
@@ -21,17 +21,13 @@ def plot_graf(x, y, title='Graf', output_path='graf.png', pontosBanco=None, pont
     return output_path
 
 
-x = np.linspace(0, 2, 100)
-y = np.cos(x)**2
+x = np.linspace(-10, 10, 100)
+y = 1/(1 + np.exp(-x))
+z = y*(1-y)
 
-# Pontos
-a1 = (0.25, 1.7)
-a2 = (0.75, 0.7)
-a3 = (1.7, 1.5)
-x1 = (1, np.cos(1)**2)
-x2 = (2, np.cos(2)**2)
-x3 = (0, np.cos(0)**2)
+a = (0, 0.25)
 
 
-figure = plot_graf(x, y, title='', output_path='cos2_graf.png', pontosBanco=[x1, x2, x3], pontosTreino=[a1, a2, a3])
+
+figure = plot_graf(x, z, title='', pontosBanco=[a], output_path='sigmoid_derivative.png')
 print(f'Figura salva em: {figure}')
